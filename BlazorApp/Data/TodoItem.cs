60a1e7f9876aabfbef653efad27ed6c1bfc079a0
QuyenTho <QuyenTho>
2021-03-22 22:52:35 +0700

    finish todo-list

---
+++ BlazorApp/Data/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Data
+{
+    public class TodoItem
+    {
+        [Required(ErrorMessage = "Please type in the task")]
+        public string TaskName { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
